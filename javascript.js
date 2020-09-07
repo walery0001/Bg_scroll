9357efdf029c3a192cd808cd0426a0dd4f6d148f
walery0001 <marcin.walery@gmail.com>
2020-09-07 09:16:23 +0200

    scroll top

--- javascript.js
+++ javascript.js
@@ -9,37 +9,42 @@ const navBtn = document.querySelectorAll(".navBtn");
 
 const btn1 = document.getElementById('menu1a');
 const btn2 = document.getElementById('menu1b');
-
+// top btn
+const topBtn = document.querySelector('.hideBtn');
+//elements height
 const heightMenu = menu2.getBoundingClientRect().height;
 const heightnavBtn = navBtnAll.getBoundingClientRect().height;
 const NavbarPlusMenu = heightMenu + heightnavBtn;
 const NavbarPlusMenu3 = heightMenu + heightMenu + heightnavBtn;
-console.log(NavbarPlusMenu)
+
+
+
+//scroll change background
 window.addEventListener('scroll', function(){
-	//const scrollMenu = window.pageYOffset;
-
-
-	const heightAllMenu = allMenu.getBoundingClientRect().top;
-
-	//console.log(scrollMenu)
-	//console.log(heightAllMenu)
-
+	//height from the top
+const allY = window.pageYOffset;
+const heightAllMenu = allMenu.getBoundingClientRect().top;
 	if(heightAllMenu < -51){
 	menu1.animate({backgroundColor:'rgba(211, 54, 54, 1)'},{duration:2000,fill:'forwards'});
 }else{	menu1.animate({backgroundColor:'rgba(82, 90, 255, 1)'},{duration:2000,fill:'forwards'});}
 
 if(heightAllMenu < -1030){
 	menu2.animate({backgroundColor:'rgba(211, 182, 54, 1)'},{duration:2000,fill:'forwards'});
-console.log(heightAllMenu)
+
 }else{	menu2.animate({backgroundColor:'rgba(211, 54, 54, 1)'},{duration:2000,fill:'forwards'});}
+
+//btn On
+if(allY > 75){
+	topBtn.classList.add('topBtn')	
+}else{
+	topBtn.classList.remove('topBtn')
+}
+//end btn top
 
 });
 
-/*navBtn.forEach(function(btn){
-	btn.addEventListener('click', function(){
-})
-	})
-*/
+
+//menus click 
 navBtn.forEach(function(btn){
 	btn.addEventListener('click', function(e){
 
@@ -50,9 +55,8 @@ const element = document.getElementById(firstMnu);
 		if(element === menu1c){window.scrollTo({left:0, top:NavbarPlusMenu3 ,behavior:"smooth"})};
 	
 	})
-
-	//window.scrollTo({left:0, top:1000 ,behavior:"smooth"});
-	
-//menu1.style.backgroundColor = 'red';
-
 });
+
+topBtn.addEventListener('click', function(){
+	window.scrollTo({left:0,top:0, behavior:'smooth'});
+})
